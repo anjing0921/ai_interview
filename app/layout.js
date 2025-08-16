a2542d89d9b035d39b9542d52cd6071cf167ac04
anjing0921 <duyuan0921@gmial.com>
2025-08-15 21:01:53 -0700

    connet the database

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><StackProvider app={stackServerApp}><StackTheme>
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </StackTheme></StackProvider></body>
     </html>
   );
