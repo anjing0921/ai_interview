9d5795b65b951cab0382af0c5ece0183fbe8f1e5
anjing0921 <duyuan0921@gmial.com>
2025-08-17 13:23:11 -0700

    add experts list

--- app/(main)/dashboard/_components/FeatureAssistants.jsx
+++ app/(main)/dashboard/_components/FeatureAssistants.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { useUser } from '@stackframe/stack'
 import { Button } from '@/components/ui/button';
+import { ExpertsList } from '@/services/Options';
+import Image from 'next/image';
+
 
 
 function FeatureAssistants() {
@@ -14,10 +17,21 @@ function FeatureAssistants() {
                     <h2 className='font-medium text-gray-500'>My Workspace</h2>
                     <h2 className='text-3xl font-bold'>Welcome back, {user?.displayName}</h2>
                 </div>
-                
                 <Button>Profile</Button>
-                
             </div>
+
+            <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 mt-10'>
+                {ExpertsList.map((option, index) => (
+                    <div key={index}>
+                        <Image src={option.icon} alt={option.name}
+                            width={210}
+                            height={180}
+                            className='h-[180px] w-[210px] hover:rotate-12 cursor-pointer transition-all'
+                        />
+                        <h2 className='mt-2'>{option.name}</h2>
+                    </div>
+                ))}
+            </div>                        
         </div>
     )
 }
---
+++ services/Options.jsx
@@ -0,0 +1,51 @@
+export const ExpertsList = [
+
+    {
+        name: 'BQ Interview',
+        icon: '/BQ.jpg',
+        prompt: 'You are a friendly AI voice interviewer simulating real interview scenarios for {user_topic}. Keep responses clear and concise. Ask structured, industry-relevant questions and provide constructive feedback to help users improve. Ensure responses stay under 120 characters.',
+        summeryPrompt: 'As per conversation give feedback to user along with where is improvment space depends in well structure',
+        abstract: '/ab2.png'
+
+    },
+    {
+        name: 'Code Interview',
+        icon: '/Code.jpg',
+        prompt: 'You are a conversational AI voice tutor helping users practice Q&A for {user_topic}. Ask clear, well-structured questions and provide concise feedback. Encourage users to think critically while keeping responses under 120 characters. Engage them with one question at a time.',
+        summeryPrompt: 'As per conversation give feedback to user along with where is improvment space depends in well structure',
+        abstract: '/ab3.png'
+    },
+    {
+        name: 'OOD Interview',
+        icon: '/OOD.jpg',
+        prompt: 'You are a helpful AI voice coach assisting users in learning {user_topic}. Provide pronunciation guidance, vocabulary tips, and interactive exercises. Keep responses friendly, engaging, and concise, ensuring clarity within 120 characters.',
+        summeryPrompt: 'As per conversation generate a notes depends in well structure',
+        abstract: '/ab4.png'
+
+    },
+];
+
+
+
+export const CoachingExpert = [
+    {
+        name: 'Joanna',
+        avatar: '/t1.avif',
+        pro: false
+    },
+    {
+        name: 'Salli',
+        avatar: '/t2.jpg',
+        pro: false
+    },
+    {
+        name: 'Joey',
+        avatar: '/t3.jpg',
+        pro: false
+    },
+    // {
+    //     name: 'Rachel',
+    //     avatar: '/t4.png',
+    //     pro: true
+    // },
+]
