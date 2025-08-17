6347f1240acbe2aa8a3f63d6e539837f8f61765c
anjing0921 <duyuan0921@gmial.com>
2025-08-16 23:30:40 -0700

    show the user's name

--- app/(main)/dashboard/_components/FeatureAssistants.jsx
+++ app/(main)/dashboard/_components/FeatureAssistants.jsx
@@ -1,9 +1,23 @@
+"use client"
 import React from 'react'
+import { useUser } from '@stackframe/stack'
+import { Button } from '@/components/ui/button';
+
 
 function FeatureAssistants() {
+    const user = useUser();
+    
     return (
         <div>
-            FeatureAssistants
+            <div className='flex justify-between items-center'>
+                <div>
+                    <h2 className='font-medium text-gray-500'>My Workspace</h2>
+                    <h2 className='text-3xl font-bold'>Welcome back, {user?.displayName}</h2>
+                </div>
+                
+                <Button>Profile</Button>
+                
+            </div>
         </div>
     )
 }
--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -6,7 +6,10 @@ function DashboardLayou({ children }) {
     return ( 
         <div>
             <AppHeader/>
-            {children}
+            <div className='p-10 mt-14 md:px-20 lg:px-32 xl:px-56 2xl:px-72'>
+                {children}
+            </div>
+            
         </div>
     
     )
