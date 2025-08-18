a1ff392caf7f3ba116a8d9b4984a018fdda38baa
anjing0921 <duyuan0921@gmial.com>
2025-08-17 18:17:51 -0700

    discussion roon done

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from 'react'
 import { useMutation, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { CoachingExpert } from '@/services/Options';
+import Image from 'next/image';
+import { UserButton } from '@stackframe/stack';
+import { Button } from '@/components/ui/button';
 
 
 function DiscussionRoom() {
@@ -21,7 +24,28 @@ function DiscussionRoom() {
     
     return (
         <div>
-            DiscussionRoom
+            <h2 className='text-lg font-bold'>{DiscussionRoomData?.coachingOption}</h2>
+            <div className='mt-5 grid grid-cols-1 lg:grid-cols-3 gap-10'>
+                <div className=' lg:col-span-2'>
+                    <div className='h-[60vh] bg-secondary border rounded-4xl
+                    flex flex-col items-center justify-center relative'>
+                        <Image src={expert?.avatar} alt='Avatar' width={200} height={200}
+                            className='h-[80px] w-[80px] rounded-full object-cover animate-pulse'/>
+                        <h2 className='text-gray-500'>{expert?.name}</h2>
+                        <div className='p-5 bg-gray-200 px-10 rounded-lg absolute bottom-10 right-10'>
+                            <UserButton />
+                        </div>
+                    </div>
+                    <div className='mt-5 flex items-center justify-center'>
+                        <Button>Connect</Button>
+                    </div>
+                </div>
+                <div className='h-[60vh] bg-secondary border rounded-4xl
+                flex flex-col items-center justify-center relative'>
+                    <h2>Chat section</h2>
+                </div>
+                <h2 className='mt-4 text-gray-400 '>At the end</h2>
+            </div>
         </div>
     )
 }
