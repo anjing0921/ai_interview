1fe43b49bb75a78d2747fae2165282f30a534894
anjing0921 <duyuan0921@gmial.com>
2025-08-21 22:19:36 -0700

    add back to dashboard button

--- app/(main)/view-summary/[roomid]/page.jsx
+++ app/(main)/view-summary/[roomid]/page.jsx
@@ -7,16 +7,24 @@ import Image from 'next/image';
 import { useParams } from 'next/navigation'
 import React from 'react'
 import SummaryBox from '../_components/SummaryBox';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation'
 
 function ViewSummary() {
     const { roomid } = useParams();
     const DiscussionRoomData = useQuery(api.DiscussionRoom.GetDiscussionRoom, { id: roomid });
     console.log(DiscussionRoomData);
+    const router = useRouter();
 
     const GetAbstractImages = (option) => {
         const coachingOption = CoachingOptions.find((item) => item.name == option)
 
         return coachingOption?.abstract ?? '/ab1.png';
+    }
+
+    const OnClickDashboard = () => {
+        console.log("Button clicked! to Dashboard")
+        router.push("/dashboard")
     }
 
     return (
@@ -38,7 +46,11 @@ function ViewSummary() {
             </div>
             <div className='grid grid-cols-1 lg:grid-cols-5 gap-5 mt-5'>
                 <div className='col-span-5'>
-                    <h2 className='text-lg font-bold mb-6'>Summary of Your Conversation</h2>
+                    <div className='grid grid-cols-3'>
+                        <h2 className='col-start-1 text-lg font-bold mb-6'>Summary of Your Conversation</h2>
+                        <Button className='col-start-3' onClick={OnClickDashboard}>Back to Dashboard</Button>
+                    </div>
+
                     <SummaryBox summary={DiscussionRoomData?.summary} />
                 </div>
             </div>
